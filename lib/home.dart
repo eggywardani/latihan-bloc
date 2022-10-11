31b4489c8a25ea59ba519f41790585e36927599e
eggywardani <eggyandikawardani@gmail.com>
2022-10-11 20:59:10 +0700

    bloc listener

--- lib/home.dart
+++ lib/home.dart
@@ -9,30 +9,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Bloc - Bloc Builder"),
+        title: Text("Flutter Bloc - Bloc Listener"),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // StreamBuilder<Object>(
-          //     stream: myCounter.stream,
-          //     initialData: myCounter.init,
-          //     builder: (context, snapshot) {
-          //       return Text(
-          //         "${snapshot.data}",
-          //         style: TextStyle(fontSize: 30),
-          //       );
-          //     }),
-          BlocBuilder<Counter, int>(
-            bloc: myCounter,
-            builder: (context, state) {
-              return Text(
-                "${state}",
-                style: TextStyle(fontSize: 30),
-              );
+          BlocListener<Counter, int>(
+            listener: (context, state) {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text("Running...")));
             },
+            listenWhen: (prev, curr) {
+              if (curr == 15) {
+                return true;
+              } else {
+                return false;
+              }
+            },
+            bloc: myCounter,
+            child: BlocBuilder<Counter, int>(
+              bloc: myCounter,
+              builder: (context, state) {
+                return Text(
+                  "${state}",
+                  style: TextStyle(fontSize: 30),
+                );
+              },
+            ),
           ),
-
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
